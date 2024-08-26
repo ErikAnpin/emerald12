01a7a4d495d6b5ba14b3e06b6e0d4212e67fd516
pls <magnusopum@hotmail.com>
2024-08-26 17:48:18 -0300

    B in wild battles to Run (voloved)

--- src/battle_controller_player.c
+++ src/battle_controller_player.c
@@ -322,6 +322,16 @@ static void HandleInputChooseAction(void)
             BtlController_EmitTwoReturnValues(BUFFER_B, B_ACTION_CANCEL_PARTNER, 0);
             PlayerBufferExecCompleted();
         }
+        else
+        {
+            if(!(gBattleTypeFlags & BATTLE_TYPE_TRAINER)) //if wild, pressing B moves cursor to run
+            {
+                PlaySE(SE_SELECT);
+                ActionSelectionDestroyCursorAt(gActionSelectionCursor[gActiveBattler]);
+                gActionSelectionCursor[gActiveBattler] = 3;
+                ActionSelectionCreateCursorAt(gActionSelectionCursor[gActiveBattler], 0);
+            }
+        }        
     }
     else if (JOY_NEW(START_BUTTON))
     {
