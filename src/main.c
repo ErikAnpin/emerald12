3827970251a486fcfbb400525143ba0778dbf088
pls <magnusopum@hotmail.com>
2024-08-26 17:50:26 -0300

    If L=A option Turbo A (devolov)

--- src/main.c
+++ src/main.c
@@ -284,7 +284,10 @@ static void ReadKeys(void)
             gMain.newKeys |= A_BUTTON;
 
         if (JOY_HELD(L_BUTTON))
+       {  
             gMain.heldKeys |= A_BUTTON;
+           gMain.newKeys ^= A_BUTTON;
+       }
     }
 
     if (JOY_NEW(gMain.watchedKeysMask))
