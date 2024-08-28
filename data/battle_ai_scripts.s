26ed92e08812d7bf043f1eee6b7329b75a68976b
pls <magnusopum@hotmail.com>
2024-08-28 10:38:38 -0300

    Disable Roamers from Fleeing (Anon822)

--- data/battle_ai_scripts.s
+++ data/battle_ai_scripts.s
@@ -3187,16 +3187,7 @@ AI_TrySunnyDayStart_End:
 	end
 
 AI_Roaming:
-	if_status2 AI_USER, STATUS2_WRAPPED, AI_Roaming_End
-	if_status2 AI_USER, STATUS2_ESCAPE_PREVENTION, AI_Roaming_End
-	get_ability AI_TARGET
-	if_equal ABILITY_SHADOW_TAG, AI_Roaming_End
-	get_ability AI_USER
-	if_equal ABILITY_LEVITATE, AI_Roaming_Flee
-	get_ability AI_TARGET
-	if_equal ABILITY_ARENA_TRAP, AI_Roaming_End
-AI_Roaming_Flee:
-	flee
+	end
 
 AI_Roaming_End:
 	end
